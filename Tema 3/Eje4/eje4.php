2b86790b269418f7eabc78c5c840aa65d7f49807
DariaKuznetsovaS <daria.kuznetsova.50758@ikasle.egibide.org>
2025-09-24 13:08:04 +0200

    Eje5

--- Tema 3/Eje4/eje4.php
+++ Tema 3/Eje4/eje4.php
@@ -1,4 +1,12 @@
 <?php
+/*04. Crea una aplicación de control de asistencia
+para un evento. La aplicación tendrá que mostrar la
+lista de personas (el nombre de cada una) que han
+acudido al evento. Tendrá un campo de texto para
+escribir el nombre de la persona y así añadir
+personas a la lista. La lista se almacenará en sesión,
+y existirá un botón que borre la lista completa.*/ 
+
 session_start();
 // se inicializa la sesion si no existe:
 if(!isset($_SESSION['asistencia'])){
@@ -9,7 +17,7 @@ if(isset($_POST['asistente'])&& !empty($_POST['asistente'])){
     $_SESSION['asistencia'][]=htmlspecialchars($asistente);
 }
 
-if(isset($_POST['borrar'])){
+if(isset($_GET['borrar'])){
     $_SESSION['asistencia']=[];
 }
 
